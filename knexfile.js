55db42423af535b8cdf2b83b4056454f6d43f5f1
Matthew Renn <rennmatthewp@gmail.com>
2018-05-21 09:56:05 -0600

    adds knex config

---
+++ knexfile.js
@@ -0,0 +1,32 @@
+module.exports = {
+  development: {
+    client: 'pg',
+    connection: 'postgres://localhost/photo_keeper',
+    migrations: {
+      directory: './db/migrations'
+    },
+    seeds: {
+      directory: './db/seeds/dev'
+    },
+    useNullAsDefault: true
+  },
+  test: {
+    client: 'pg',
+    connection: 'postgres://localhost/photo_keeper_test',
+    migrations: {
+      directory: './db/migrations'
+    },
+    seeds: {
+      directory: './db/seeds/dev'
+    },
+    useNullAsDefault: true
+  },
+  production: {
+    client: 'pg',
+    connection: process.env.DATABASE_URL + `?ssl=true`,
+    migrations: {
+      directory: './db/migrations'
+    },
+    useNullAsDefault: true
+  }
+};
