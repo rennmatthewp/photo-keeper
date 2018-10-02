c6f11690ef2067387b50077f9371d3e5724cba0d
Matthew Renn <rennmatthewp@gmail.com>
2018-10-02 11:50:58 -0600

    Adds CORS

--- server.js
+++ server.js
@@ -6,6 +6,22 @@ const server = express();
 
 server.set('port', process.env.PORT || 3000);
 
+server.use((request, response, next) => {
+  response.header(
+    'Access-Control-Allow-Origin',
+    '*',
+  );
+  response.header(
+    'Access-Control-Allow-Headers',
+    'Content-Type, Origin, X-Requested-With, Accept',
+  );
+  response.header(
+    'Access-Control-Allow-Methods',
+    'GET,POST,PATCH,DELETE',
+  );
+  next();
+});
+
 server.use(bodyParser.json());
 server.use('/', express.static('public'));
 server.use('/api/v1', router);
